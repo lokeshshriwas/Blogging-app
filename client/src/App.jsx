fc2c4f612777d509441756f1dc53bb06a989f2e6
lokesh shriwas <lokeshshriwas2802@gmail.com>
2024-03-04 23:18:02 +0530

    created pages from react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import SignUp from "./pages/SignUp"
+import SignIn from "./pages/SignIn"
+import Dashboard from "./pages/Dashboard"
+import Projects from "./pages/Projects"
+
+
 function App() {
   return (
-   <div></div>
+   <BrowserRouter>
+    <Routes>
+      <Route exact path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/sign-up" element={<SignUp/>}/>
+      <Route path="/sign-in" element={<SignIn/>}/>
+      <Route path="/dashboard" element={<Dashboard/>}/>
+      <Route path="/projects" element={<Projects/>}/>
+    </Routes>
+   </BrowserRouter>
   )
 }
 
