9cee152d85f1e1557235c09d53bac4fe0b7c7b46
lokesh shriwas <lokeshshriwas2802@gmail.com>
2024-03-06 17:35:55 +0530

    added privacy route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,31 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import SignUp from "./pages/SignUp"
-import SignIn from "./pages/SignIn"
-import Dashboard from "./pages/Dashboard"
-import Projects from "./pages/Projects"
-import Header from "./components/Header"
-import FooterComp from "./components/Footer.jsx"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import Header from "./components/Header";
+import FooterComp from "./components/Footer.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx";
 
 function App() {
   return (
-   <BrowserRouter>
-   <Header/>
-    <Routes>
-      <Route exact path="/" element={<Home/>}/>
-      <Route path="/about" element={<About/>}/>
-      <Route path="/sign-up" element={<SignUp/>}/>
-      <Route path="/sign-in" element={<SignIn/>}/>
-      <Route path="/dashboard" element={<Dashboard/>}/>
-      <Route path="/projects" element={<Projects/>}/>
-    </Routes>
-    <FooterComp/>
-   </BrowserRouter>
-  )
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+      <FooterComp />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
